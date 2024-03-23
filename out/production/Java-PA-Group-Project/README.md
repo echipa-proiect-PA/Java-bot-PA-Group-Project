e27789846c9a3a36a6285150d4f51afaf0b68d32
madalin-spoiala <dev@madalinspoiala.com>
2024-03-23 07:47:19 +0200

    deleted unnecessary files

--- out/production/Java-PA-Group-Project/README.md
+++
@@ -1,3 +0,0 @@
-# Membri
-- Trifan Bogdan Cristian, 322 CD
-- Valerica-Madalin Spoiala, 342 C4
--- test/etapa-1/environment/make.bat
+++
@@ -1,1 +0,0 @@
-g++ -o2 -std=c++11 main.cpp core/Halite.cpp -I . networking/Networking.cpp -o halite.exe -static
